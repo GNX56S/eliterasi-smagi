634ce22d035cd125a3132cd7f56c668378bc96e6
Hannn <ilhanjuliansyah3@gmail.com>
2024-05-17 21:30:48 +0700

    mengganti side bar
    -mengubah style sidebar
    -menambahkan pop up tulis artikel(belum fix)

---
+++ src/sidebarDaftarArtikel.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+    const toggleBtn = document.getElementById('toggleBtn');
+    const toggleIcon = document.getElementById('toggleIcon');
+
+    toggleBtn.addEventListener('click', () => {
+        if (sidebar.classList.contains('w-64')) {
+            sidebar.classList.replace('w-64', 'w-16');
+            toggleIcon.classList.replace('fa-xmark', 'fa-bars');
+            document.querySelectorAll('#sidebar span').forEach(span => span.classList.add('hidden'));
+            document.querySelectorAll('.profil-guru').forEach(li => li.classList.add('hidden'));
+            document.querySelectorAll('.side-text').forEach(a => a.classList.replace('justify-start','justify-center'));
+        } else {
+            sidebar.classList.replace('w-16', 'w-64');
+            toggleIcon.classList.replace('fa-bars', 'fa-xmark');
+            document.querySelectorAll('#sidebar span').forEach(span => span.classList.remove('hidden'));
+            document.querySelectorAll('.profil-guru').forEach(li => li.classList.remove('hidden'));
+            document.querySelectorAll('.side-text').forEach(a => a.classList.replace('justify-center','justify-start'));
+        }
+    });
+});
+
+window.openModal = function(modalId) {
+    document.getElementById(modalId).style.display = 'block'
+    document.getElementsByTagName('body')[0].classList.add('overflow-y-hidden')
+}
+
+window.closeModal = function(modalId) {
+    document.getElementById(modalId).style.display = 'none'
+    document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+}
+
+// Close all modals when press ESC
+document.onkeydown = function(event) {
+    event = event || window.event;
+    if (event.keyCode === 27) {
+        document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+        let modals = document.getElementsByClassName('modal');
+        Array.prototype.slice.call(modals).forEach(i => {
+            i.style.display = 'none'
+        })
+    }
+};
