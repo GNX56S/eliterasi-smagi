2c92ee65b2967e0bd14a2282313c37327671bb70
Hannn <ilhanjuliansyah3@gmail.com>
2024-05-18 18:52:18 +0700

    menambahkan rich text editor

--- src/sidebarDaftarArtikel.js
+++ src/sidebarDaftarArtikel.js
@@ -30,14 +30,4 @@ window.closeModal = function(modalId) {
     document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
 }
 
-// Close all modals when press ESC
-document.onkeydown = function(event) {
-    event = event || window.event;
-    if (event.keyCode === 27) {
-        document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
-        let modals = document.getElementsByClassName('modal');
-        Array.prototype.slice.call(modals).forEach(i => {
-            i.style.display = 'none'
-        })
-    }
-};
+
